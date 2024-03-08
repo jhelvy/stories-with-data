49236050fdb7c6559046f627d7e8ddd41ae15bef
emse-p4a-gwu <john.helveston@gmail.com>
2024-03-08 11:27:05 -0500

    rendered slides for each parts

--- render.R
+++ render.R
@@ -4,15 +4,13 @@ library(renderthis)
 # Render slides 
 parts <- c('storytelling', 'visualizing-information')
 for (part in parts) {
-    to_html(
-        from = file.path('parts', part, 'index.Rmd'),
-        to = file.path('parts', part, 'index.html')
-    )
-    to_pdf(
-        from = file.path('parts', part, 'index.html'), 
-        to = file.path('parts', part, paste0(part, '.pdf')), 
-        partial_slides = TRUE
-    )
+    path_rmd <- file.path('parts', part, 'index.Rmd')
+    path_html <- file.path('parts', part, 'index.html')
+    path_pdf <- file.path('parts', part, paste0(part, '.pdf'))
+    to_html(from = path_rmd, to = path_html)
+    to_pdf(from = path_html, to = path_pdf, partial_slides = TRUE)
+    # Compress the PDF to reduce size
+    tools::compactPDF(path_pdf, gs_quality = 'ebook')
 }
 
 # Render site
